a240598b2f56ec1e4349019c3683f24b74083379
John Johansen <john.johansen@canonical.com>
2010-06-23 14:38:22 -0700

    AppArmor: Don't redefine ACC_MODE
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/include/file.h
+++ security/apparmor/include/file.h
@@ -201,7 +201,6 @@ static inline void aa_free_file_rules(struct aa_file_rules *rules)
 #define ACC_FMODE(x) (("\000\004\002\006"[(x)&O_ACCMODE]) | (((x) << 1) & 0x40))
 
 /* from namei.c */
-#define ACC_MODE(x) ("\000\004\002\006"[(x)&O_ACCMODE])
 #define MAP_OPEN_FLAGS(x) ((((x) + 1) & O_ACCMODE) ? (x) + 1 : (x))
 /*
  * map file flags to AppArmor permissions
