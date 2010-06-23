93c0c65cb49603013fd7d08d8766d503ec4dcd69
John Johansen <john.johansen@canonical.com>
2010-06-23 14:38:23 -0700

    AppArmor: Update comments in context
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/include/context.h
+++ security/apparmor/include/context.h
@@ -30,11 +30,21 @@ struct aa_file_cxt {
 	u16 allowed;
 };
 
+/**
+ * aa_alloc_file_context - allocate file_cxt
+ * @gfp: gfp flags for allocation
+ *
+ * Returns: file_cxt or NULL on failure
+ */
 static inline struct aa_file_cxt *aa_alloc_file_context(gfp_t gfp)
 {
 	return kzalloc(sizeof(struct aa_file_cxt), gfp);
 }
 
+/**
+ * aa_free_file_context - free a file_cxt
+ * @cxt: file_cxt to free  (MAYBE_NULL)
+ */
 static inline void aa_free_file_context(struct aa_file_cxt *cxt)
 {
 	aa_put_profile(cxt->profile);
@@ -43,9 +53,9 @@ static inline void aa_free_file_context(struct aa_file_cxt *cxt)
 
 /**
  * struct aa_task_cxt - primary label for confined tasks
- * @profile: the current profile
- * @exec: profile to transition to on next exec
- * @previous: profile the task may return to
+ * @profile: the current profile   (NOT NULL)
+ * @exec: profile to transition to on next exec  (MAYBE NULL)
+ * @previous: profile the task may return to     (MAYBE NULL)
  * @token: magic value the task must know for returning to @previous_profile
  *
  * Contains the task's current profile (which could change due to
@@ -71,7 +81,7 @@ int aa_restore_previous_profile(u64 cookie);
 
 /**
  * __aa_task_is_confined - determine if @task has any confinement
- * @task: task to check confinement of
+ * @task: task to check confinement of  (NOT NULL)
  *
  * If @task != current needs to be called in RCU safe critical section
  */
@@ -88,9 +98,9 @@ static inline bool __aa_task_is_confined(struct task_struct *task)
 
 /**
  * aa_cred_profile - obtain cred's profiles
- * @cred: cred to obtain profiles from
+ * @cred: cred to obtain profiles from  (NOT NULL)
  *
- * Returns: system confining profile
+ * Returns: confining profile
  *
  * does NOT increment reference count
  */
@@ -104,7 +114,7 @@ static inline struct aa_profile *aa_cred_profile(const struct cred *cred)
 /**
  * __aa_current_profile - find the current tasks confining profile
  *
- * Returns: up to date confining profile or the ns unconfined profile
+ * Returns: up to date confining profile or the ns unconfined profile (NOT NULL)
  *
  * This fn will not update the tasks cred to the most up to date version
  * of the profile so it is safe to call when inside of locks.
@@ -117,7 +127,7 @@ static inline struct aa_profile *__aa_current_profile(void)
 /**
  * aa_current_profile - find the current tasks confining profile and do updates
  *
- * Returns: up to date confinging profile or the ns unconfined profile
+ * Returns: up to date confining profile or the ns unconfined profile (NOT NULL)
  *
  * This fn will update the tasks cred structure if the profile has been
  * replaced.  Not safe to call inside locks
