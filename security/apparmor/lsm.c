4c4f31e16ab72a229e4ba697ec06c2f1d6bf5f7a
John Johansen <john.johansen@canonical.com>
2010-06-23 14:38:28 -0700

    AppArmor: If exec succeeds set the files cached permissions as xmr
    Prior to the previous files creds patch, files opened for exec did
    not cache a profile and implicitly granted, m and r permissions
    after a successful exec check.
    
    With the file creds caching patch, this implicit grant of permissions
    breaks.  For files that have successfully passed the exec checks
    cache the granted permissions as rmx (read, may map executable, executable).
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -413,17 +413,25 @@ static int apparmor_inode_getattr(struct vfsmount *mnt, struct dentry *dentry)
 
 static int apparmor_dentry_open(struct file *file, const struct cred *cred)
 {
+	struct aa_file_cxt *fcxt = file->f_security;
 	struct aa_profile *profile;
 	int error = 0;
 
-	/* If in exec, permission is handled by bprm hooks */
-	if (current->in_execve ||
-	    !mediated_filesystem(file->f_path.dentry->d_inode))
+	if (!mediated_filesystem(file->f_path.dentry->d_inode))
 		return 0;
+
+	/* If in exec, permission is handled by bprm hooks.
+	 * Cache permissions granted by the previous exec check, with
+	 * implicit read and executable mmap which are required to
+	 * actually execute the image.
+	 */
+	if (current->in_execve) {
+		fcxt->allow = MAY_EXEC | MAY_READ | AA_EXEC_MMAP;
+		return 0;
+	}
 
 	profile = aa_cred_profile(cred);
 	if (!unconfined(profile)) {
-		struct aa_file_cxt *fcxt = file->f_security;
 		struct inode *inode = file->f_path.dentry->d_inode;
 		struct path_cond cond = { inode->i_uid, inode->i_mode };
 
