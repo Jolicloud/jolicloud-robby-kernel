10fca726d368798ca1f2e7b170ddec52a4268aca
John Johansen <john.johansen@canonical.com>
2010-06-23 14:38:24 -0700

    AppArmor: Fix allocation of cred_blank
    Fix the inverted error condition allocation of blank cred.
    
    See: 
    http://marc.info/?l=linux-kernel&m=126687686719210&w=2
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -59,7 +59,7 @@ static int apparmor_cred_alloc_blank(struct cred *cred, gfp_t gfp)
 {
 	/* freed by apparmor_cred_free */
 	struct aa_task_cxt *cxt = aa_alloc_task_context(gfp);
-	if (cxt)
+	if (!cxt)
 		return -ENOMEM;
 
 	cred->security = cxt;
