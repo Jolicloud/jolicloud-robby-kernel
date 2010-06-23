109cfe0db2904967bcf5f79457af25a9bacfb0e7
John Johansen <john.johansen@canonical.com>
2010-06-23 14:38:23 -0700

    AppArmor: Update commenting in apparmorfs
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -25,6 +25,15 @@
 #include "include/context.h"
 #include "include/policy.h"
 
+/**
+ * kvmalloc - do allocation prefering kmalloc but falling back to vmalloc
+ * @size: size of allocation
+ *
+ * Return: allocated buffer or NULL if failed
+ *
+ * It is possible that policy being loaded from the user is larger than
+ * what can be allocated by kmalloc, in those cases fall back to vmalloc.
+ */
 static void *kvmalloc(size_t size)
 {
 	void *buffer;
@@ -38,14 +47,34 @@ static void *kvmalloc(size_t size)
 	return buffer;
 }
 
+/**
+ * kvfree - free an allocation do by kvmalloc
+ * @buffer: buffer to free
+ *
+ * Free a buffer allocated by kvmalloc
+ */
 static void kvfree(void *buffer)
 {
+	if (!buffer)
+		return;
+
 	if (is_vmalloc_addr(buffer))
 		vfree(buffer);
 	else
 		kfree(buffer);
 }
 
+/**
+ * aa_simple_write_to_buffer - common routine for getting policy from user
+ * @userbuf: user buffer to copy data from  (NOT NULL)
+ * @alloc_size: size of user buffer
+ * @copy_size: size of data to copy from user buffer
+ * @pos: position write is at in the file
+ * @operation: name of operation doing the user buffer copy (NOT NULL)
+ *
+ * Returns: kernel buffer containing copy of user buffer data or an
+ *          ERR_PTR on failure.
+ */
 static char *aa_simple_write_to_buffer(const char __user *userbuf,
 				       size_t alloc_size, size_t copy_size,
 				       loff_t *pos, const char *operation)
@@ -91,7 +120,8 @@ out:
 	return data;
 }
 
-/* apparmor/.load */
+
+/* .load file hook fn to load policy */
 static ssize_t aa_profile_load(struct file *f, const char __user *buf,
 			       size_t size, loff_t *pos)
 {
@@ -113,7 +143,7 @@ static const struct file_operations aa_fs_profile_load = {
 	.write = aa_profile_load
 };
 
-/* apparmor/.replace */
+/* .replace file hook fn to load and/or replace policy */
 static ssize_t aa_profile_replace(struct file *f, const char __user *buf,
 				  size_t size, loff_t *pos)
 {
@@ -135,7 +165,7 @@ static const struct file_operations aa_fs_profile_replace = {
 	.write = aa_profile_replace
 };
 
-/* apparmor/.remove */
+/* .remove file hook fn to remove loaded policy */
 static ssize_t aa_profile_remove(struct file *f, const char __user *buf,
 				 size_t size, loff_t *pos)
 {
@@ -166,12 +196,13 @@ static const struct file_operations aa_fs_profile_remove = {
 
 /**
  * __next_namespace - find the next namespace to list
- * @root: root namespace to stop search at
- * @ns: current ns position
+ * @root: root namespace to stop search at (NOT NULL)
+ * @ns: current ns position (NOT NULL)
  *
- * Find the next namespace and to list and handle all locking needed
+ * Find the next namespace from @ns under @root and handle all locking needed
  * while switching current namespace.
  *
+ * Returns: next namespace or NULL if at last namespace under @root
  * NOTE: will not unlock root->lock
  */
 static struct aa_namespace *__next_namespace(struct aa_namespace *root,
@@ -187,6 +218,7 @@ static struct aa_namespace *__next_namespace(struct aa_namespace *root,
 		return next;
 	}
 
+	/* check if the next ns is a sibling, parent, gp, .. */
 	parent = ns->parent;
 	while (parent) {
 		read_unlock(&ns->lock);
@@ -205,8 +237,10 @@ static struct aa_namespace *__next_namespace(struct aa_namespace *root,
 
 /**
  * __first_profile - find the first profile in a namespace
- * @root: namespace that is root of profiles being displayed
- * @ns: namespace to start in
+ * @root: namespace that is root of profiles being displayed (NOT NULL)
+ * @ns: namespace to start in   (NOT NULL)
+ *
+ * Returns: unrefcounted profile or NULL if no profile
  */
 	static struct aa_profile *__first_profile(struct aa_namespace *root,
 						  struct aa_namespace *ns)
@@ -221,7 +255,7 @@ static struct aa_namespace *__next_namespace(struct aa_namespace *root,
 
 /**
  * __next_profile - step to the next profile in a profile tree
- * @profile: current profile in tree
+ * @profile: current profile in tree (NOT NULL)
  *
  * Perform a depth first taversal on the profile tree in a namespace
  *
@@ -257,8 +291,8 @@ static struct aa_profile *__next_profile(struct aa_profile *p)
 
 /**
  * next_profile - step to the next profile in where ever it may be
- * @root: root namespace
- * @profile: current profile
+ * @root: root namespace  (NOT NULL)
+ * @profile: current profile  (NOT NULL)
  *
  * Returns: next profile or NULL if there isn't one
  */
@@ -277,6 +311,8 @@ static struct aa_profile *next_profile(struct aa_namespace *root,
  * p_start - start a depth first traversal of profile tree
  * @f: seq_file to fill
  * @pos: current position
+ *
+ * Returns: first profile under current namespace or NULL if none found
  *
  * acquires first ns->lock
  */
@@ -300,6 +336,16 @@ static void *p_start(struct seq_file *f, loff_t *pos)
 	return profile;
 }
 
+/**
+ * p_next - read the next profile entry
+ * @f: seq_file to fill
+ * @p: profile previously returned
+ * @pos: current position
+ *
+ * Returns: next profile after @p or NULL if none
+ *
+ * may acquire/release locks in namespace tree as necessary
+ */
 static void *p_next(struct seq_file *f, void *p, loff_t *pos)
 {
 	struct aa_profile *profile = p;
@@ -314,8 +360,7 @@ static void *p_next(struct seq_file *f, void *p, loff_t *pos)
  * @f: seq_file we are filling
  * @p: the last profile writen
  *
- * if we haven't completely traversed the profile tree will release the
- * locking.
+ * Release all locking done by p_start/p_next on namespace tree
  */
 static void p_stop(struct seq_file *f, void *p)
 	__releases(root->lock)
@@ -351,7 +396,13 @@ static bool print_ns_name(struct seq_file *f, struct aa_namespace *root,
 	return 1;
 }
 
-/* Returns: error on failure */
+/**
+ * seq_show_profile - 
+ * @f: seq_file to file
+ * @p: current position (profile)    (NOT NULL)
+ *
+ * Returns: error on failure
+ */
 static int seq_show_profile(struct seq_file *f, void *p)
 {
 	struct aa_profile *profile = (struct aa_profile *)p;
