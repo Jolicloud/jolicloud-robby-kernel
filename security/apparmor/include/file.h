722f01e9021167da11159a873c8209471d7373de
John Johansen <john.johansen@canonical.com>
2010-07-12 04:55:12 -0700

    AppArmor: Remap file perms to take advantage of 32 bit perms
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/include/file.h
+++ security/apparmor/include/file.h
@@ -37,10 +37,10 @@ struct aa_profile;
 #define AA_EXEC_MMAP                   0x0800
  
 #define AA_MAY_LINK			0x1000
-#define AA_MAY_ONEXEC			0x4000	/* exec allows onexec */
-#define AA_MAY_CHANGE_PROFILE		0x8000
 #define AA_LINK_SUBSET			AA_MAY_LOCK	/* overlayed */
-#define AA_MAY_CHANGEHAT		0x8000	/* ctrl auditing only */
+#define AA_MAY_ONEXEC			0x40000000	/* exec allows onexec */
+#define AA_MAY_CHANGE_PROFILE		0x80000000
+#define AA_MAY_CHANGEHAT		0x80000000	/* ctrl auditing only */
 
 #define AA_AUDIT_FILE_MASK	(MAY_READ | MAY_WRITE | MAY_EXEC | MAY_APPEND |\
 				 AA_MAY_CREATE | AA_MAY_DELETE |	\
