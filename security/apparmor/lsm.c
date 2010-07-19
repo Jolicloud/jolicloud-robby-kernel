97e3e55578dddec84a18d42e8499d09b26745204
John Johansen <john.johansen@canonical.com>
2010-07-19 15:00:42 +0200

    AppArmor: Make apparmor_capable easier to read by cleaning up if cond
    As per Tetsuo Handa and Serge Hallyn
    http://marc.info/?l=linux-kernel&m=127925772724163&w=2
    http://marc.info/?l=linux-kernel&m=127921475703669&w=2
    
    The if condition in apparmor_capable can be cleaned up by reworking the
    code slightly.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -141,11 +141,11 @@ static int apparmor_capable(struct task_struct *task, const struct cred *cred,
 	struct aa_profile *profile;
 	/* cap_capable returns 0 on success, else -EPERM */
 	int error = cap_capable(task, cred, cap, audit);
-
-	profile = aa_cred_profile(cred);
-	if (!error  && !unconfined(profile))
-		error = aa_capable(task, profile, cap, audit);
-
+	if (!error) {
+		profile = aa_cred_profile(cred);
+		if (!unconfined(profile))
+			error = aa_capable(task, profile, cap, audit);
+	}
 	return error;
 }
 
