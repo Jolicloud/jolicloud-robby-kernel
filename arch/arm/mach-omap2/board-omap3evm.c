c97113e60293384f46b220e53f0664971a794020
Mathieu J. Poirier <mathieu.poirier@canonical.com>
2010-09-15 17:14:01 -0600

    UBUNTU: SAUCE: Adding vdd_sdi regulator supply to OMAP3EVM
    https://bugs.launchpad.net/bugs/630885
    
    When booting, the omapdss subsystem is looking for a regulator named
    "vdds_sdi". When the regulator is not found the initialisation sequence
    is aborted resulting in omapfb not finding a display to work with.  This
    patch allows the omapfb sub system to complete its initialisation
    properly and enable LCD display. The problem was fixed by lumping a
    "vdds_sdi" with the already existing "vdds_dsi" regulator. This fix
    takes its root from work done on the Beagle board and the Pandora board.
    More spefically:
    
     - 7b097896e4a5b5ea4798db806e63a1138b1b8eb8
     - f6873eedd94df20cfb705856a78440a4c176c6b6
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@canonical.com>
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

--- arch/arm/mach-omap2/board-omap3evm.c
+++ arch/arm/mach-omap2/board-omap3evm.c
@@ -514,9 +514,9 @@ static struct regulator_init_data omap3_evm_vdac = {
 };
 
 /* VPLL2 for digital video outputs */
-static struct regulator_consumer_supply omap3_evm_vpll2_supply = {
-	.supply		= "vdvi",
-	.dev		= &omap3_evm_lcd_device.dev,
+static struct regulator_consumer_supply omap3_evm_vdds_supplies[] = {
+	REGULATOR_SUPPLY("vdds_sdi", "omapdss"),
+	REGULATOR_SUPPLY("vdds_dsi", "omapdss"),
 };
 
 static struct regulator_init_data omap3_evm_vpll2 = {
@@ -530,8 +530,8 @@ static struct regulator_init_data omap3_evm_vpll2 = {
 		.valid_ops_mask		= REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 	},
-	.num_consumer_supplies	= 1,
-	.consumer_supplies	= &omap3_evm_vpll2_supply,
+	.num_consumer_supplies	= ARRAY_SIZE(omap3_evm_vdds_supplies),
+	.consumer_supplies	= omap3_evm_vdds_supplies,
 };
 
 static struct twl4030_platform_data omap3evm_twldata = {
